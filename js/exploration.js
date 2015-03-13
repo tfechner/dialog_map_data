76b6061cae419876e7f236cfdabd88ce28e228bf
Thore Fechner <t.fechner@uni-muenster.de>
2015-03-13 15:40:47 +0100

    First Working Version

--- js/exploration.js
+++ js/exploration.js
@@ -2,7 +2,7 @@ var url = 'https://api.keen.io/3.0/projects/54b6884e96773d36ffcb1d4a/queries/ext
 
 
 var width = $(window).width(),
-    height = 200,
+    height = 50,
 	barHeight = 20,
 	barPadding = 5;
 	
@@ -62,67 +62,57 @@ var color_page =["#784C20","#CB4B23","#CD9433","#CC7E4F"]
 		
 		});
 	});
+
+
+prepare_divs(nested_keen);
+
+
+function prepare_divs(nested_keen) {
+	var divs = d3.select("body")
+	.selectAll("div")
+	.data(nested_keen,function(d){
+		//console.log(d);
+		return d;})
+	.data(nested_keen)
 	
-	draw(nested_keen[15]);
-	draw(nested_keen[60]);
-	draw(nested_keen[90]);
+	.enter()
+	.append("div")
+	.attr("id", function(d) {
+		console.log(d);
+		return "fingerprint_"+d.key});
+	//.text(function(d) { 
+	//	return "fingerprint_"+d.key});
 	
-//});
+	//.append("svg")
+	
+	nested_keen.forEach(function (d,i) {
+		draw(nested_keen[i]);
+	});
+};
 
 function draw(keen_data_fingerprint) {
 	
-	var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-    //.call(zoom);
-	
-	var container = svg.append("g");
-	
+	var container = d3.select("#fingerprint_"+keen_data_fingerprint.key)
+	.append("svg")
+	.attr("width", width)
+    .attr("height", height)
+	.append("g");
+
 	var keen_data = keen_data_fingerprint.values
-	//As we have sorted the data by timestamp we don't have to find the min and max =
+
 	var x = d3.time.scale()
     .domain([keen_data[0].timestamp,keen_data[keen_data.length-1].next_timestamp])
-    // .range([0, width-(keen_data.length*20)]);
     .range([0, width]);
 	
-	/*keen_data.forEach(function (d,i) {
-		console.log( x(keen_data[i].next_timestamp) - x(keen_data[i].timestamp) );
-	});
-	*/
+
 	
 	
 	var group = container.selectAll("g")
     .data(keen_data)
     .enter().append("g")
 	.attr("class", "chart")	
-	.attr("transform", function(d, i) { return "translate("+10*i+",0)"; })
-	.on("mouseover", function (d) {
-		//showTooltip(d, this);
-	})
-    .on("mouseout", function () {		
-		//d3.select("#tooltip").classed("hidden", true);;
-	});
-	
-	group.append("circle")
-	.attr("cx", function(d) { return x(d.timestamp) } )
-    //.attr("height", barHeight - 1);
-	.attr("cy",barHeight)
-	.attr("r", 2);
-	
-	group.append("g")
-    .attr("transform", function(d, i) { return "translate("+ x(d.timestamp) +","+ barHeight +") rotate(45) "; })
-	.append("text")
-    .attr("x", 0)
-    .attr("y", 8)
-    //.attr("dy", ".35em")
-    .text(function(d) { 
-	if (d.action == "loadPage") return d.url;
-	else if  (d.action == "sidebar:contributionMouseEnter") return "Sidebar Mouse Enter: "+d.contribution;
-	else if  (d.action == "sidebar:contributionMouseLeave") return "Sidebar Mouse Leave: "+d.contribution;
-	else if  (d.action == "sidebar:mouseoverFeatureTag") return "Mouse Over Feature Tag :"+d.tag.properties.title;
-	else if  (d.action == "sidebar:clickReply") return "Sidebar Click Reply: "+d.contribution_id; 
-	else return d.action;
-	});
+	.attr("transform", function(d, i) { return "translate("+0*i+",0)"; });
+
 	
 	group.append("g")
 	.attr("transform", function(d, i) { return "translate("+(x(d.timestamp)) +",0)"; })
@@ -132,7 +122,7 @@ function draw(keen_data_fingerprint) {
 	.attr("fill", function(d) {
             return setRectColor(d,this);
     });
-    svg.transition().call(zoom.scale($(window).width()/container.node().getBBox().width).event);
+    container.transition().call(zoom.scale($(window).width()/container.node().getBBox().width).event);
 
 }
 
@@ -162,7 +152,7 @@ function setRectColor(d, context) {
 
 
 function zoomed() {
-  container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+  //container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
 
 
@@ -202,4 +192,74 @@ function replacer(key,value)
 	else if (key=="marker-color") return undefined;
 	else if (key=="id") return undefined;
 	else return value;
+}
+
+function draw_old(keen_data_fingerprint, div_id) {
+	
+	var svg = d3.select("body")
+	.append("div")
+    //.attr("id", function(d) { return d.label; })
+	.append("svg")
+    .attr("width", width)
+    .attr("height", height);
+    //.call(zoom);
+	
+	var container = svg.append("g");
+	
+	var keen_data = keen_data_fingerprint.values
+	//As we have sorted the data by timestamp we don't have to find the min and max =
+	var x = d3.time.scale()
+    .domain([keen_data[0].timestamp,keen_data[keen_data.length-1].next_timestamp])
+    // .range([0, width-(keen_data.length*20)]);
+    .range([0, width]);
+	
+	/*keen_data.forEach(function (d,i) {
+		console.log( x(keen_data[i].next_timestamp) - x(keen_data[i].timestamp) );
+	});
+	*/
+	
+	
+	var group = container.selectAll("g")
+    .data(keen_data)
+    .enter().append("g")
+	.attr("class", "chart")	
+	.attr("transform", function(d, i) { return "translate("+3*i+",0)"; })
+	.on("mouseover", function (d) {
+		//showTooltip(d, this);
+	})
+    .on("mouseout", function () {		
+		//d3.select("#tooltip").classed("hidden", true);;
+	});
+	
+	/*group.append("circle")
+	.attr("cx", function(d) { return x(d.timestamp) } )
+    //.attr("height", barHeight - 1);
+	.attr("cy",barHeight)
+	.attr("r", 2);
+	
+	group.append("g")
+    .attr("transform", function(d, i) { return "translate("+ x(d.timestamp) +","+ barHeight +") rotate(45) "; })
+	.append("text")
+    .attr("x", 0)
+    .attr("y", 8)
+    //.attr("dy", ".35em")
+    .text(function(d) { 
+	if (d.action == "loadPage") return d.url;
+	else if  (d.action == "sidebar:contributionMouseEnter") return "Sidebar Mouse Enter: "+d.contribution;
+	else if  (d.action == "sidebar:contributionMouseLeave") return "Sidebar Mouse Leave: "+d.contribution;
+	else if  (d.action == "sidebar:mouseoverFeatureTag") return "Mouse Over Feature Tag :"+d.tag.properties.title;
+	else if  (d.action == "sidebar:clickReply") return "Sidebar Click Reply: "+d.contribution_id; 
+	else return d.action;
+	});
+	*/
+	group.append("g")
+	.attr("transform", function(d, i) { return "translate("+(x(d.timestamp)) +",0)"; })
+	.append("rect")
+	.attr("width", function(d) { return (x(d.next_timestamp) - x(d.timestamp)) } )
+	.attr("height", barHeight - 1)
+	.attr("fill", function(d) {
+            return setRectColor(d,this);
+    });
+    svg.transition().call(zoom.scale($(window).width()/container.node().getBBox().width).event);
+
 }
